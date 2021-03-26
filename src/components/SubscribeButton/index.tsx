a4a54ac36cb006848319d1a8f506c7c50aa71623
viniciusoliveiras <vinitag190@gmail.com>
2021-03-26 11:17:26 -0300

    Payment on Stripe | Redirecting to Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import { useSession, signIn } from 'next-auth/client';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
@@ -6,12 +8,24 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  function handleSubscribe() {
-    const [session] = useSession();
+  const [session] = useSession();
 
+  async function handleSubscribe() {
     if (!session) {
       signIn('github');
       return;
+    }
+
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
     }
   }
 
