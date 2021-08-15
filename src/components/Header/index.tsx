9c875bf0d265a4e21c00e26bf95a5dba00469333
viniciusoliveiras <vinitag190@gmail.com>
2021-08-15 12:06:47 -0300

    style: header and home page 425px responssiveness

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,18 @@ export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news logo" />
+        <img
+          src="/images/logo.svg"
+          alt="ig.news logo"
+          className={styles.logoDesktop}
+        />
+
+        <img
+          src="/reactjs-line.svg"
+          alt="ig.news logo"
+          className={styles.logoMobile}
+        />
+
         <nav>
           <ActiveLink activeClassName={styles.active} href="/">
             <a>Home</a>
